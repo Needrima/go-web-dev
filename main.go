4e63c77873c4cbdc81a59762338d40c2bcbe5b5e
Oyebode Amirdeen <oyebodeamirdeen@gmail.com>
2020-07-01 01:20:57 +0100

    Added TCP server1

---
+++ main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"net"
+)
+
+func main() {
+	ln, err := net.Listen("tcp", ":8082")
+	if err != nil {
+		log.Fatalln("Error listening:", err)
+	}
+	defer ln.Close()
+
+	for {
+		conn, err := ln.Accept()
+		if err != nil {
+			log.Fatalln("Error connecting:", err)
+		}
+		io.WriteString(conn, "Hey!, I'm ready to listen\n")
+		fmt.Fprintln(conn, "\t\toya now i don ready to listen.")
+		conn.Close()
+	}
+}
