5f49226454fba5ff0059bc74c9cad6ef7fb2a299
Oyebode Amirdeen <oyebodeamirdeen@gmail.com>
2020-08-24 21:08:04 +0100

    sessions file added

---
+++ sessions/encrypting-password/main.go
@@ -0,0 +1,114 @@
+package main
+
+import (
+	"fmt"
+	"github.com/satori/go.uuid"
+	"text/template"
+	"net/http"
+	"strconv"
+	"golang.org/x/crypto/bcrypt"
+)
+
+var tpl *template.Template
+
+type user struct {
+	Sname, Fname, Oname, Email, Username string
+	Age                                            int
+	Password []byte
+}
+
+var sessiondb = map[string]string{}
+var userdb = map[string]user{}
+
+func init() {
+	tpl = template.Must(template.ParseFiles("index.html", "signup.html"))
+}
+
+func LoggedIn(r *http.Request) bool { //to check if user is already logged in
+	cookie, err := r.Cookie("session")
+	if err != nil { //no cookie found; meaning user is not logged in
+		return false
+	}
+	//when user is logged in
+	pw := sessiondb[cookie.Value]
+	_, ok := userdb[pw]
+	return ok // user found; meaning user is logged in
+}
+
+func signup(w http.ResponseWriter, r *http.Request) {
+	if LoggedIn(r) { //User cannot signup when already logged in
+		http.Error(w, "You are logged in already", http.StatusBadRequest)
+		http.Redirect(w, r, "/", http.StatusSeeOther)
+		return
+	}
+
+	//Process form data
+	if r.Method == http.MethodPost {
+		sn := r.FormValue("sname")
+		fn := r.FormValue("fname")
+		on := r.FormValue("oname")
+		em := r.FormValue("email")
+		un := r.FormValue("uname")
+		pw := r.FormValue("pword")
+		ag, err := strconv.Atoi(r.FormValue("age")) //convert age to int
+		if err != nil {                             //Error occurs when text is passed into age field
+			http.Error(w, "Age field allows numbers only", http.StatusInternalServerError)
+			http.Redirect(w, r, "/signup", http.StatusSeeOther)
+			return
+		} else if ag < 15 { //Age restriction
+			http.Error(w, "Age requirement not met", http.StatusInternalServerError)
+			http.Redirect(w, r, "/signup", http.StatusSeeOther)
+			return
+		}
+
+		//encrypt password
+		bs, err := bcrypt.GenerateFromPassword([]byte(pw), bcrypt.MinCost)
+		if err != nil {
+			fmt.Println("Error encrypting password:", err)
+			return
+		}
+
+		//check if password already exist
+		_, ok := userdb[pw]
+		if ok {
+			http.Error(w, "Password taken by another user", http.StatusInternalServerError)
+			http.Redirect(w, r, "/", http.StatusTemporaryRedirect)
+			return
+		}
+		//create cookie and set cookie
+		sessionID, _ := uuid.NewV4()
+		cookie := &http.Cookie{
+			Name:     "session",
+			Value:    sessionID.String(),
+			HttpOnly: true,
+		}
+		http.SetCookie(w, cookie)
+
+		//create user
+		u := user{sn, fn, on, em, un, ag, bs}
+
+		//print user info
+		fmt.Println("User info:", u)
+
+		//store user info
+		sessiondb[cookie.Value] = pw
+		userdb[pw] = u
+
+		//redirect back to home page after storing user info
+		http.Redirect(w, r, "/", http.StatusSeeOther)
+		return
+	}
+
+	tpl.ExecuteTemplate(w, "signup.html", nil)
+}
+
+func Home(w http.ResponseWriter, r *http.Request) {
+	tpl.ExecuteTemplate(w, "index.html", nil)
+}
+
+func main() {
+	http.HandleFunc("/home", Home)
+	http.HandleFunc("/signup", signup)
+	http.Handle("/favicon.ico", http.NotFoundHandler())
+	http.ListenAndServe(":8080", nil)
+}
---
+++ sessions/passing-data-between-routes-using-cookies/main.go
@@ -0,0 +1,76 @@
+package main
+
+import (
+	"fmt"
+	"github.com/satori/go.uuid"
+	"html/template"
+	"net/http"
+	"os"
+)
+
+var tpl *template.Template
+
+type user struct {
+	Username, Firstname, Othername string
+}
+//Both database variables below are global scope so they can be called in different functions
+var sessiondb = map[string]string{} // maps session id to user id
+var userdb = map[string]user{}      // maps user id to user
+
+func init() {
+	tpl = template.Must(template.ParseFiles("index.html", "home.html"))
+}
+
+func foo(w http.ResponseWriter, r *http.Request) {
+	cookie, err := r.Cookie("Session") //Get session cookie
+	if err != nil {                    //if cookie does not exist create cookie and set it
+		sessionID, err := uuid.NewV4()
+		if err != nil {
+			fmt.Fprintln(os.Stdout, "Error generating sessionID")
+		}
+		cookie = &http.Cookie{
+			Name:     "Session",
+			Value:    sessionID.String(),
+			HttpOnly: true,
+			//Secure : true,
+		}
+		http.SetCookie(w, cookie)
+	}
+	//if cookie exist
+	var u user //create user
+
+	// get data from form
+	if r.Method == http.MethodPost {
+		un := r.FormValue("username")
+		f := r.FormValue("firstname")
+		o := r.FormValue("othername")
+		u = user{un, f, o} //create a user from the form data that is to be passed into the index template on line 48
+		//this is where the storage happens
+		sessiondb[cookie.Value] = un // map the value of the username to the cookie value since they are both unique
+		userdb[un] = u // map the value of the user info to the username since username is unique
+	}
+	tpl.ExecuteTemplate(w, "index.html", u) //pass the user info on line 45 to the index template
+}
+
+func bar(w http.ResponseWriter, r *http.Request) {
+	cookie, err := r.Cookie("Session") // get cookie with this name
+	if err != nil {
+		http.Redirect(w, r, "/foo", http.StatusTemporaryRedirect)
+		return
+	}
+	//search for the uname using cookie value in the sessions db
+	uname, ok := sessiondb[cookie.Value]
+	if !ok { //if uname does not exist redirect
+		http.Redirect(w, r, "/foo", http.StatusSeeOther)
+		return
+	}
+	user := userdb[uname] //search for the user info using the unmae in the user db
+	tpl.ExecuteTemplate(w, "home.html", user) //pass the user info into the home index
+}
+
+func main() {
+	http.HandleFunc("/foo", foo)
+	http.HandleFunc("/bar", bar)
+	http.Handle("/favicon.ico", http.NotFoundHandler())
+	http.ListenAndServe(":8080", nil)
+}
