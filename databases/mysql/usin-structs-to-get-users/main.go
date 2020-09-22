bc0a34e8a9de2fe76596840177a29dcdf33128c3
Oyebode Amirdeen <oyebodeamirdeen@gmail.com>
2020-09-22 19:28:26 +0100

    Updated database folder

--- databases/mysql/usin-structs-to-get-users/main.go
+++ databases/mysql/usin-structs-to-get-users/main.go
@@ -4,21 +4,59 @@ import (
 	"database/sql"
 	"fmt"
 	_ "github.com/go-sql-driver/mysql"
+	"net/http"
+	"text/template"
 )
 
 type user struct {
-	Firstname
+	Firstname, Surname string
+	ID                 int
+}
+
+var tpl *template.Template
+var db *sql.DB
+
+func init() {
+	tpl = template.Must(template.ParseFiles("index.html"))
 }
 
 func main() {
-	fmt.Println("My SQL Database")
+	fmt.Println("My SQL Database Names")
 	//"root:demopassword@tcp(127.0.0.1:3306)/usersdb" follows the pattern
 	//"name_of_connection:password@tcp("connection_host")/database_name"
-	db, err := sql.Open("mysql", "root:Ademola15@tcp(127.0.0.1:3306)/usersdb")
+	var err error
+	db, err = sql.Open("mysql", "root:password@tcp(127.0.0.1:3306)/usersdb")
 	if err != nil {
 		fmt.Println("Could not connect to mysql workbench:", err)
 	}
-
 	defer db.Close()
 
+	http.HandleFunc("/users", Index)
+	http.Handle("/favicon.ico", http.NotFoundHandler())
+	http.ListenAndServe(":8080", nil)
+}
+
+func Index(w http.ResponseWriter, r *http.Request) {
+	rows, err := db.Query("select Firstname, Surname, ID from signup")
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	var users []user
+
+	for rows.Next() {
+		var u user
+		err := rows.Scan(&u.Firstname, &u.Surname, &u.ID)
+		if err != nil {
+			fmt.Println(err)
+		}
+
+		users = append(users, u)
+	}
+
+	for _, us := range users {
+		fmt.Println(us.Firstname, us.Surname, us.ID)
+	}
+
+	tpl.Execute(w, users)
 }
