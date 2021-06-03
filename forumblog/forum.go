339e71477871206fbe3e9d5bc46a58196b3d7844
Amirdeen Oyebode <oyebodeamirdeen@gmail.com>
2021-06-03 01:19:05 -0700

    commit

--- forumblog/forum.go
+++ forumblog/forum.go
@@ -15,17 +15,19 @@ import (
 	"gopkg.in/mgo.v2/bson"
 )
 
-var forumCollection *mgo.Collection // mongoDB collection
+var blogPostCollection *mgo.Collection // mongoDB collection
+var commentsCollection *mgo.Collection
+var repliesCollection *mgo.Collection
 
 var tpl *template.Template
 
 type Reply struct {
-	Author, Reply string
+	Replier, Reply, ID, BelongsTo string
 }
 
 type Comment struct {
-	Author, Comment, ID, ReplyRoute string
-	Replies                         []string
+	Commentor, Comment, ID, BelongsTo, ReplyRoute string
+	Replies                                       []Reply
 }
 
 type BlogPost struct {
@@ -61,14 +63,15 @@ func main() {
 	err = session.Ping()
 	Check(err, "Connection to database failed")
 
-	forumCollection = session.DB("golang").C("go_practice")
-
+	blogPostCollection = session.DB("golang").C("blogPost")
+	commentsCollection = session.DB("golang").C("comments")
+	repliesCollection = session.DB("golang").C("replies")
 	//routes
 
 	http.HandleFunc("/", Home)
 	http.HandleFunc("/new-post", newPost)
 	http.HandleFunc("/post/", PostSingle)
-	//http.HandleFunc("/reply/", Reply)
+	http.HandleFunc("/reply/", replyToComment)
 
 	log.Println("Now serving on port 8080.....")
 
@@ -80,7 +83,7 @@ func main() {
 func Home(w http.ResponseWriter, r *http.Request) {
 	var blogPosts []BlogPost
 
-	err := forumCollection.Find(bson.M{}).All(&blogPosts)
+	err := blogPostCollection.Find(bson.M{}).All(&blogPosts)
 	Check(err, "Finding blogposts")
 
 	tpl.ExecuteTemplate(w, "home.html", blogPosts)
@@ -91,7 +94,7 @@ func newPost(w http.ResponseWriter, r *http.Request) {
 	var blogPosts []BlogPost
 	var IDs []string
 
-	err := forumCollection.Find(bson.M{}).All(&blogPosts)
+	err := blogPostCollection.Find(bson.M{}).All(&blogPosts)
 	Check(err, "Find in newpost")
 
 	for _, v := range blogPosts {
@@ -123,7 +126,7 @@ func newPost(w http.ResponseWriter, r *http.Request) {
 
 		newPost := BlogPost{Author: author, Title: title, Message: message, ID: IDAsString, SingleRoute: singleRoute} // create new post
 
-		err = forumCollection.Insert(&newPost) // store newpost
+		err = blogPostCollection.Insert(&newPost) // store newpost
 		Check(err, "Inserting blogpost")
 
 		tpl.ExecuteTemplate(w, "post.html", "Thread Created")
@@ -139,11 +142,16 @@ func PostSingle(w http.ResponseWriter, r *http.Request) {
 	path := r.URL.Path // get path
 
 	PostId := path[11:] // trim to get post Id
-	fmt.Println(PostId)
+	// fmt.Println("Post Id:",PostId)
 
 	var bp BlogPost // get blogpost from DB using ID
-	err := forumCollection.Find(bson.M{"id": PostId}).One(&bp)
+	err := blogPostCollection.Find(bson.M{"id": PostId}).One(&bp)
 	Check(err, "Find in PostSingle")
+
+	var Comments []Comment
+	err = commentsCollection.Find(bson.M{"belongsto": PostId}).All(&Comments)
+	Check(err, "Finding comments in PostSingle")
+	bp.Comments = Comments
 
 	if r.Method == http.MethodGet {
 		blogPostAndPath := postAndPath{bp, path}
@@ -151,46 +159,88 @@ func PostSingle(w http.ResponseWriter, r *http.Request) {
 	} else if r.Method == http.MethodPost { // user trying to comment
 		name := r.FormValue("author")
 		message := r.FormValue("message")
-
-		// get former comments ID
-		var blogPosts []BlogPost
-		var CommentIDs []string
-
-		err := forumCollection.Find(bson.M{}).All(&blogPosts)
-		Check(err, "Find in postSingle")
-
-		for _, v := range blogPosts {
-			for _, v2 := range v.Comments {
-				CommentIDs = append(CommentIDs, v2.ID)
-			}
-		}
-		//define ID for new comment
 		rand.Seed(time.Now().UnixNano())
 		ID := rand.Intn(99999) + 1
 		IDAsString := strconv.Itoa(ID)
 
 		// check if ID is not already taken
+		var comment Comment
 		for {
-			if !Found(CommentIDs, IDAsString) {
-				break
-			} else {
-				if ID > 100000 {
-					ID = 0
-				}
+			if ID > 100000 {
+				ID = 0
 			}
+
+			err := commentsCollection.Find(bson.M{"id": IDAsString}).One(&comment)
+			if err != nil {
+				break
+			}
+
 			ID++
 		}
 
 		replyRoute := "comment-" + IDAsString
 
-		comment := Comment{Author: name, Comment: message, ReplyRoute: replyRoute}
+		newcomment := Comment{Commentor: name, Comment: message, ID: IDAsString, ReplyRoute: replyRoute, BelongsTo: PostId}
 
-		bp.Comments = append(bp.Comments, comment) // update comment
-
-		err = forumCollection.Update(bson.M{"id": PostId}, bson.M{"$set": bson.M{"comments": bp.Comments}}) // update comment in DB
+		err = commentsCollection.Insert(&newcomment) // update comment in DB
 		Check(err, "Updating comment")
 
-		blogPostAndPath := postAndPath{bp, path}
-		tpl.ExecuteTemplate(w, "singlePost.html", blogPostAndPath)
+		http.Redirect(w, r, path, http.StatusSeeOther)
+	}
+}
+
+type commentAndPath struct {
+	Comment
+	Path string
+}
+
+func replyToComment(w http.ResponseWriter, r *http.Request) {
+	path := r.URL.Path
+	fmt.Println("reply path:", path)
+
+	commentId := path[15:]
+	fmt.Println("Comment id:", commentId)
+
+	var comment Comment
+	err := commentsCollection.Find(bson.M{"id": commentId}).One(&comment)
+	Check(err, "Finding Comment in replytocomment")
+	fmt.Println("comment:", comment)
+
+	var RepliesToComment []Reply
+	err = repliesCollection.Find(bson.M{"belongsto": commentId}).All(&RepliesToComment)
+	Check(err, "Finding Replies in replytocomment")
+	comment.Replies = RepliesToComment
+
+	if r.Method == http.MethodGet {
+		commentWithPath := commentAndPath{comment, path}
+		tpl.ExecuteTemplate(w, "reply.html", commentWithPath)
+	} else if r.Method == http.MethodPost {
+		name := r.FormValue("name")
+		message := r.FormValue("message")
+		rand.Seed(time.Now().UnixNano())
+		ID := rand.Intn(999999) + 1
+		IDAsString := strconv.Itoa(ID)
+
+		// check if ID is not already taken
+		var reply Reply
+		for {
+			if ID > 1000000 {
+				ID = 0
+			}
+
+			err := repliesCollection.Find(bson.M{"id": IDAsString}).One(&reply)
+			if err != nil {
+				break
+			}
+
+			ID++
+		}
+
+		newreply := Reply{Replier: name, Reply: message, ID: IDAsString, BelongsTo: commentId}
+
+		err = repliesCollection.Insert(&newreply) // update comment in DB
+		Check(err, "Updating comment")
+
+		http.Redirect(w, r, path, http.StatusSeeOther)
 	}
 }
