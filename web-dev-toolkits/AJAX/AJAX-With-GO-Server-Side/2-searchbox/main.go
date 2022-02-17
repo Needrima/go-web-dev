ab0e8a8821f3ecc95a903872e71089cf5bbe51af
oyebode amirdeen <oyebodeamirdeen@gmail.com>
2022-02-17 05:58:38 -0800

    commit

--- web-dev-toolkits/AJAX/AJAX-With-GO-Server-Side/2-searchbox/main.go
+++ web-dev-toolkits/AJAX/AJAX-With-GO-Server-Side/2-searchbox/main.go
@@ -1,12 +1,48 @@
 package main
 
 import (
+	"context"
 	"encoding/json"
 	"fmt"
 	"html/template"
 	"io/ioutil"
+	"log"
 	"net/http"
+	"os"
+
+	"go.mongo.org/mongo-driver/bson"
+	"go.mongo.org/mongo-driver/mongo"
+	"go.mongo.org/mongo-driver/mongo/primitive"
+	// "go.mongo.org/mongo-driver/mongo/readpref"
+	"go.mongo.org/mongo-driver/mongo/options"
 )
+
+type autoComplete struct {
+	Data []string `bson:"data, omitempty"`
+}
+
+var testCollection *mongo.Collection
+
+func init() {
+	clientOptions := options.Client().ApplyURI("mongodb://locahost:27017")
+
+	connection, err := mongo.Connect(clientOptions)
+	if err != nil {
+		log.Printf("Error connecting to mongoDB server: %w\n", err)
+		os.Exit(1)
+	}
+
+	defer connection.Disconnect(context.TODO())
+
+	if err := connection.Ping(); err != nil {
+		log.Printf("Pinging DB failed: %w\n", err)
+		os.Exit(1)
+	}
+
+	db := connection.Database("gocode")
+
+	testCollection = db.Collection("test")
+}
 
 func main() {
 	tpl := template.Must(template.ParseFiles("index.html"))
@@ -16,7 +52,20 @@ func main() {
 	})
 
 	http.HandleFunc("/foo", func(w http.ResponseWriter, r *http.Request) {
-		searchResults := []string{"one", "two", "three", "four", "five", "six"}
+		var aut autoComplete
+
+		id := primitive.ObjectIdFromHex("6205295f1eea720fd0d0ef68")
+
+		if err := testCollection.FindOne(
+			context.TODO(),
+			bson.M{"_id": id},
+		).Decode(&aut); err != nil {
+			log.Println("ERROR FINDING RESULT:", err)
+			return
+		}
+
+		fmt.Println(aut.Data)
+		searchResults := aut.Data
 
 		bs, err := ioutil.ReadAll(r.Body)
 		if err != nil {
